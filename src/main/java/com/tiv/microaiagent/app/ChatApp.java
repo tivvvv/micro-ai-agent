e62a13f9890416369b5d016bb9b0b30a88a5c984
tivvvv <18612553935@163.com>
2025-05-14 22:18:35 +0800

    feat: 实现支持对话记忆的聊天功能.

--- src/main/java/com/tiv/microaiagent/app/ChatApp.java
+++ src/main/java/com/tiv/microaiagent/app/ChatApp.java
@@ -6,9 +6,12 @@ import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
 
 @Slf4j
 @Component
@@ -16,14 +19,36 @@ public class ChatApp {
 
     private final ChatClient chatClient;
 
-    @Value("${app.default.system.prompt}")
-    private String DEFAULT_SYSTEM_PROMPT = "";
+    private String DEFAULT_SYSTEM_PROMPT = "你是java专家";
 
+    /**
+     * 初始化对话客户端
+     *
+     * @param chatModel
+     */
     public ChatApp(@Qualifier("dashscopeChatModel") ChatModel chatModel) {
         ChatMemory chatMemory = new InMemoryChatMemory();
         chatClient = ChatClient.builder(chatModel)
                 .defaultSystem(DEFAULT_SYSTEM_PROMPT)
                 .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build();
+    }
+
+    /**
+     * 基础对话
+     *
+     * @param msg
+     * @param chatId
+     * @return
+     */
+    public String doChat(String msg, String chatId) {
+        ChatResponse chatResponse = chatClient.prompt()
+                .user(msg)
+                .advisors(advisorSpec -> advisorSpec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId).param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        String result = chatResponse.getResult().getOutput().getText();
+        log.info("result: {}", result);
+        return result;
     }
 }
---
+++ src/test/java/com/tiv/microaiagent/app/ChatAppTest.java
@@ -0,0 +1,34 @@
+package com.tiv.microaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class ChatAppTest {
+
+    @Resource
+    private ChatApp chatApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String msg = "你好,我是超哥,想做java开发";
+        String answer = chatApp.doChat(msg, chatId);
+        Assertions.assertNotNull(answer);
+
+        // 第二轮
+        msg = "进入大厂做后端开发,需要掌握哪些内容";
+        answer = chatApp.doChat(msg, chatId);
+        Assertions.assertNotNull(answer);
+
+        // 第三轮
+        msg = "我的名字是什么?刚刚跟你说了,帮我回忆一下";
+        answer = chatApp.doChat(msg, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
